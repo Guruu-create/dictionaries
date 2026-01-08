607d1cebf33e018e9a2cd1618adfd3d43badf5ff
Guru <prasadrajarapu12@gmail.com>
2026-01-08 20:22:47 +0530

    Add files via upload

---
+++ dict5.py
@@ -0,0 +1,16 @@
+fruits = {
+    1 : "apple",
+    2 : "mango",
+    3 : "banana",
+    4 : "kiwi",
+    5 : "cherry"
+}
+
+#removing items
+# pop()
+fruits.pop(5)
+print(fruits)
+
+# popitem() removes the last item
+fruits.popitem()
+print(fruits)
